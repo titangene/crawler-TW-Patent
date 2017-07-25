24d3512c63e3dfb7dad56c11a7f942291505c114
titangene <titangene01@gmail.com>
2017-07-25 14:22:00 +0800

    Add connection mysql, input value(print pages, item per page)

---
+++ common.rb
@@ -0,0 +1,25 @@
+require 'capybara'
+require 'mysql2'
+
+OPTIONS = {
+  :host     => 'localhost', # 主機
+  :username => 'root',      # 使用者名稱
+  :password => 'titan',     # 密碼
+  :database => 'tw_patent', # 資料庫
+  :encoding => 'utf8'       # 編碼
+}
+
+def _sleep(t, t_max, css)
+  time = 0
+  while !page.has_selector?(css) do
+    sleep(t)
+    time += t
+    puts "#{css} - Zzz... #{time} sec"
+    break if time > t_max
+  end
+
+  if time > t_max
+    puts "Sleep more than #{t_max} seconds to stop the crawler"
+    exit
+  end
+end
--- tw_patent.rb
+++ tw_patent.rb
@@ -1,5 +1,6 @@
-require 'capybara'
+require_relative 'common'
 require 'capybara/dsl'
+require 'pry-byebug'
 include Capybara::DSL
 
 Capybara.javascript_driver = :selenium
@@ -8,13 +9,38 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
+client = Mysql2::Client.new(OPTIONS)
+SQL = "insert into crawler(id, name, application_date, IPC, LOC, 
+inventor, applicant, reference, patent_start_date, patent_stop_date) 
+values(?, ?, ?, ?, ?, ?, ?, ?, null, null)"
+@db_insert = client.prepare(SQL)
+
+print_pages = ARGV[0]
+print_pages ||= 1
+print_pages = print_pages.to_i
+
+@items_per_page = ARGV[1]
+@items_per_page ||= 10
+@items_per_page = @items_per_page.to_i
+
+if @items_per_page != 10 && @items_per_page != 20 && @items_per_page != 30 &&
+   @items_per_page != 40 && @items_per_page != 50 && @items_per_page != 100
+  puts "value 2: Please input items per page (10/20/30/40/50/100), Default: 10"
+  exit
+end
+
+if print_pages == 1
+  puts "value 1: Please input the number of pages printed (optional), Default: 1"
+  puts "value 2: Please input items per page (10/20/30/40/50/100), Default: 10"
+end
+
 # 中華民國專利資訊檢索系統 首頁
 url = "http://twpat2.tipo.gov.tw/tipotwoc/tipotwkm"
 visit(url)
 
 # 進入 簡易檢索 頁面
 find('ul#css3menu1 li.topmenu:nth-child(3) a').click
-sleep(3)
+_sleep(3, 21, 'table.TERM_0_11_S')
 
 # 找出專利公開日在 2016/1/1~2017/1/1 之間的所有專利
 # 年：1950 ~ 2017
@@ -32,28 +58,39 @@ patent_fields.each do |patent_field|
   patent_field.set(true)
 end
 # 每頁顯示筆數：10/20/30/40/50/100
-@items_per_page = 20
 find_field('_0_7_o_1').find("option[value='#{@items_per_page}']").click
 # 開始搜尋
 page.execute_script("document.getElementsByName('_IMG_檢索2%m')[0].click()")
-sleep(3)
+_sleep(3, 21, 'tr.sumtr1')
 
 def print_Patents()
   # 列印所有專利資料
   patents = all('tr.sumtr1')
   patents.each_with_index do |patent, index|
     _no = index + 1 + (@p_page - 1) * @items_per_page
+    id = patent.find('td.sumtd2_PN a').text
+    name = patent.find('td.sumtd2_TI').text
+    application_date = patent.find('td.sumtd2_AD').text
+    ipc = patent.find('td.sumtd2_IC').text
+    loc = patent.find('td.sumtd2_IQ').text
+    inventor = patent.find('td.sumtd2_IV').text
+    applicant = patent.find('td.sumtd2_PA').text
+    reference = patent.find('td.sumtd2_CI').text
+
+    @db_insert.execute(id, name, application_date, ipc, loc, inventor, 
+      applicant, reference)
+
     puts "-- No. #{_no} ------------------------"
-    puts "專利編號：" + patent.find('td.sumtd2_PN a').text
-    # puts "專利名稱：" + patent.find('td.sumtd2_TI').text
-    # puts "申請日：" + patent.find('td.sumtd2_AD').text
-    # puts "國際分類號/IPC：" + patent.find('td.sumtd2_IC').text
-    # puts "設計分類號/LOC：" + patent.find('td.sumtd2_IQ').text
-    # puts "發明人：" + patent.find('td.sumtd2_IV').text
-    # puts "申請人：" + patent.find('td.sumtd2_PA').text
-    # puts "參考文獻：" + patent.find('td.sumtd2_CI').text
-    # puts "專利權始日：" + patent.find('td.sumtd2_ID').text
-    # puts "專利權止日：" + patent.find('td.sumtd2_ID').text
+    puts "專利編號：" + id
+    # puts "專利名稱：" + name
+    # puts "申請日：" + application_date
+    # puts "國際分類號/IPC：" + IPC
+    # puts "設計分類號/LOC：" + LOC
+    # puts "發明人：" + inventor
+    # puts "申請人：" + applicant
+    # puts "參考文獻：" + reference
+    # puts "專利權始日：" + patent.find('td.sum').text
+    # puts "專利權止日：" + patent.find('td.sum').text
   end
 end
 
@@ -74,8 +111,8 @@ puts "共 #{all_page} 頁"
 @p_page = 1
 print_Patents()
 
+
 # 爬到各分頁的專利資料
-print_pages = 2
 print_pages--   # 前面已做一次，所以要先 -1
 i = 1
 while i < print_pages do
